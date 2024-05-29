f07930020a21c44717de7bdbf66c1e8b8197124b
ProgSomel <somelahmed88@gmail.com>
2024-05-29 15:29:05 +0600

    Home----> Connects NonProfit Section Done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,18 +1,97 @@
 import Banner from "../Banner/Banner";
 
-
 const Home = () => {
-    return (
+  return (
+    <div>
+      <Banner></Banner>
+      {/* Connects Non Profit  */}
+      <div className="flex flex-col md:flex-row py-20 lg:py-32 px-12 gap-12">
         <div>
-            <Banner></Banner>
+          <h1 className="font-poetsen text-3xl ">
+            Connects Nonprofits, <br />
+            Donors, & Companies <br />
+            in Every Country
+          </h1>
         </div>
-    );
+        {/* Cards  */}
+        <div className="flex flex-col md:flex-row  gap-12 lg:w-3/4">
+          {/* Cards 1 */}
+          <div className="space-y-12">
+            <div className="flex gap-2">
+              <div>
+                <img
+                className="w-[200px]"
+                  src="https://bighearts.wgl-demo.net/wp-content/uploads/2020/08/icon-box_05.png"
+                  alt=""
+                />
+              </div>
+              <div className="">
+                <h1 className="font-poetsen text-2xl mb-2">Healthy Food</h1>
+                <p className="font-mono  ">
+                  We help local nonprofits access the funding, tools, training,
+                  and support they need to become more.
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div>
+                <img
+                className="w-[200px]"
+                  src="https://bighearts.wgl-demo.net/wp-content/uploads/2020/08/icon-box_07.png"
+                  alt=""
+                />
+              </div>
+              <div className="">
+                <h1 className="font-poetsen text-2xl mb-2">Medical Help</h1>
+                <p className="font-mono  ">
+                  We help local nonprofits access the funding, tools, training,
+                  and support they need to become more.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Cards 2  */}
+          <div className="">
+            <div className="space-y-12">
+              <div className="flex gap-2">
+                <div>
+                  <img
+                  className="w-[200px]"
+                    src="https://bighearts.wgl-demo.net/wp-content/uploads/2020/08/icon-box_06.png"
+                    alt=""
+                  />
+                </div>
+                <div className="">
+                  <h1 className="font-poetsen text-2xl mb-2">Clean Water</h1>
+                  <p className="font-mono  ">
+                    We help local nonprofits access the funding, tools,
+                    training, and support they need to become more.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2">
+                <div>
+                  <img
+                  className="w-[200px]"
+                    src="https://bighearts.wgl-demo.net/wp-content/uploads/2020/08/icon-box_08.png"
+                    alt=""
+                  />
+                </div>
+                <div className="">
+                  <h1 className="font-poetsen text-2xl mb-2">Education</h1>
+                  <p className="font-mono  ">
+                  We help local nonprofits access the funding, tools, training, and support they need to become more.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-
-Home.propTypes = {
-
-};
-
+Home.propTypes = {};
 
 export default Home;
--- src/pages/Home/Slide/Slide.jsx
+++ src/pages/Home/Slide/Slide.jsx
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types";
 import bgImg from "../../../assets/Banner/bg.png";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
+
 
 const Slide = ({ image, h1, text }) => {
+  useEffect(()=> {
+    AOS.init({
+      duration: 1000,
+    })
+  }, [])
   return (
     <div>
       <div
@@ -21,7 +30,7 @@ const Slide = ({ image, h1, text }) => {
             animation: 'float 10s infinite alternate'
           }}
         >
-          <div className="flex justify-center items-center min-h-screen flex-col space-y-5">
+          <div data-aos="fade-up" className="flex justify-center items-center min-h-screen flex-col space-y-5">
             <h1 className="text-white text-xl md:text-3xl  font-semibold font-pacifico italic shadow-xl opacity-[600px] text-center uppercase">{h1}</h1>
             <p className="text-3xl md:text-5xl lg:text-7xl text-center font-poetsen font-bold text-white shadow-xl opacity-[600px]">{text}</p>
           </div>
